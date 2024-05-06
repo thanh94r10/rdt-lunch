bcfc96a2498ee370fe86cf9f5266dfc82218b28b
Nguyen Quoc Thanh <thanh.nq1@umc.edu.vn>
2024-05-06 11:14:42 +0700

    new release

--- assets/getRTLPlacement--oQHo_13.js
+++
@@ -1,1 +0,0 @@
-import{v as e}from"./DefaultLayout-B1IAJ90r.js";var a=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{a as g};
