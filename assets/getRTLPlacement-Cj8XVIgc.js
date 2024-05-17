22c628b5e45370595d77160a4f7e26e4ff40993b
Nguyen Quoc Thanh <thanh.nq1@umc.edu.vn>
2024-05-17 08:11:29 +0700

    new changes

--- assets/getRTLPlacement-Cj8XVIgc.js
+++
@@ -1,1 +0,0 @@
-import{v as e}from"./DefaultLayout-DJ4j9LBL.js";var a=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{a as g};
